d2896f50e4a5d47e8311958a800b13b90cda4c28
yayajack123 <43714571+yayajack123@users.noreply.github.com>
2020-03-31 19:40:03 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <HTML> 
-<HEAD> 
+<HEAD>
+  <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">  
 <TITLE>COVID19</TITLE> 
 </HEAD> 
 <BODY> 
 <!-- Begin Click2Map Widget -->
-<iframe id="c2m_map_widget" src="https://www.click2map.com/v2/yuniaswandewi/Penyebaran_COVID19_di_Denpasar_Selatan/widget" width="800" height="500" frameborder="0" sytle="display:block;"></iframe>
+<iframe id="c2m_map_widget" src="https://www.click2map.com/v2/yuniaswandewi/Penyebaran_COVID19_di_Denpasar_Selatan/widget" width="800" height="800" frameborder="0" sytle="display:block;"></iframe>
 <!-- End Click2Map Widget -->
 </BODY> 
 </HTML>
