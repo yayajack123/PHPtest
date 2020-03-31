ec699c102e1f0ba282ec77565c5c440ed4b8ffa7
yayajack123 <43714571+yayajack123@users.noreply.github.com>
2020-03-31 18:53:42 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-halo nama saya jack
+<HTML> 
+<HEAD> 
+<TITLE>COVID19</TITLE> 
+</HEAD> 
+<BODY> 
+<!-- Begin Click2Map Widget -->
+<iframe id="c2m_map_widget" src="https://www.click2map.com/v2/yuniaswandewi/Penyebaran_COVID19_di_Denpasar_Selatan/widget" width="500" height="500" frameborder="0"></iframe>
+<!-- End Click2Map Widget -->
+</BODY> 
+</HTML>
+
